15eae04b9a3d658532504d15e83c388a3dcd748c
Isiko Saidiali <isikosaidiali@gmail.com>
2024-10-05 09:01:43 +0300

    posthog installed

--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -1,6 +1,7 @@
 import {RemixBrowser} from '@remix-run/react';
-import {startTransition, StrictMode} from 'react';
+import {startTransition, StrictMode, useEffect} from 'react';
 import {hydrateRoot} from 'react-dom/client';
+import posthog from 'posthog-js';
 
 if (!window.location.origin.includes('webcache.googleusercontent.com')) {
   startTransition(() => {
@@ -8,7 +9,19 @@ if (!window.location.origin.includes('webcache.googleusercontent.com')) {
       document,
       <StrictMode>
         <RemixBrowser />
+        <PosthogInit />
       </StrictMode>,
     );
   });
+}
+
+function PosthogInit() {
+  useEffect(() => {
+    posthog.init('phc_F4xC9pLkcC9iCBkVI2x6qa8qHGhA3GfUWq8iY9ayfOg', {
+      api_host: 'https://us.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    });
+  }, []);
+
+  return null;
 }
